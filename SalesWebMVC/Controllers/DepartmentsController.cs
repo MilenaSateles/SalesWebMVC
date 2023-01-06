461ef4067e01e3ea27704fd2617cf131e535e61a
Milena Sateles <milenamcsateles@gmail.com>
2023-01-06 13:57:05 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "MakeUp" });
+            list.Add(new Department { Id = 2, Name = "Nails" });
+
+            return View(list);
+        }
+    }
+}
