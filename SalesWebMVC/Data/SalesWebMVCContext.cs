ae5ff20a9f63cd17c2332056ed25d42a1aade670
Milena Sateles <milenamcsateles@gmail.com>
2023-01-09 09:21:51 -0300

    Other entities and second migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -13,7 +13,9 @@ namespace SalesWebMVC.Data
             : base(options)
         {
         }
-
         public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecords { get; set; } = default!;
+
     }
 }
